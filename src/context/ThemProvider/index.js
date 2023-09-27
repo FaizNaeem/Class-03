81879e9215de7a5a2bbb5e4f95493975cab0611d
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-27 18:41:04 +0500

    object

--- src/context/ThemProvider/index.js
+++ src/context/ThemProvider/index.js
@@ -2,7 +2,10 @@
 import React ,{createContext, useState}from 'react'
  export  const ThemeContext = createContext()
  export default function ThemProvider({children}){
-    const [theme, SetTheme] = useState('gray')
+    const [theme, SetTheme] = useState({
+        red :"red",
+        yellow : "yellow"
+    })
     return(
         <ThemeContext.Provider value={{theme,SetTheme}}>
             {children}
